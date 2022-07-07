bc53b4c208931551c6021671b073abd5cc670ea9
Marcus Noble <AverageMarcus@users.noreply.github.com>
2022-07-07 10:28:12 +0100

    Added make target to generate values schema (#73)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- `schema-gen` Make target for generating values schema
+
 ## [1.3.0] - 2022-07-07
 
 ### Changed
--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -27,3 +27,10 @@ verify: generate
 		git diff; \
 		echo "generated files are out of date, run make generate"; exit 1; \
 	fi
+
+ensure-schema-gen:
+	@helm schema-gen --help &>/dev/null || helm plugin install https://github.com/mihaisee/helm-schema-gen.git
+
+.PHONY: schema-gen
+schema-gen: ensure-schema-gen ## Generates the values schema file
+	@cd helm/cluster-api && helm schema-gen values.yaml > values.schema.json && git diff values.schema.json
